89fccae709eeaa5b990561752236e6656cefd137
Eunidb <a352110133@alumno.uttehuacan.edu.mx>
2023-01-30 12:31:48 -0600

    first commit

---
+++ src/app/components/componentes/componente.component.ts
@@ -0,0 +1,14 @@
+import { components } from '@angular/core';
+import { AppComponent } from '../../app.component';
+
+@components(
+  {
+    selector: 'AppComponent',
+    template: '',
+  }
+)
+
+export class componentecomponent {
+
+
+}
